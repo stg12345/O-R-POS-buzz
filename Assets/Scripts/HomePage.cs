278428c5e830a8f874f6bed9831eb70507ac39cf
Swapnil S. Patil <swap_nil12@yahoo.com>
2014-07-21 23:41:43 +0530

    first commit

---
+++ Assets/Scripts/HomePage.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HomePage : MonoBehaviour {
+	public GUITexture playbutton;
+	public GUITexture ratingstar;
+	public GUITexture leaderboard;
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began) 
+		{
+			if (playbutton.HitTest(Input.GetTouch(0).position))
+			{
+				//testtouch.text = "touched";
+				Application.LoadLevel("MainLevelDemo");
+			}
+			if(leaderboard.HitTest(Input.GetTouch(0).position))
+			{
+
+			}
+
+			if(ratingstar.HitTest(Input.GetTouch(0).position))
+			{
+				Application.OpenURL ("http://www.google.com");
+			}
+		}
+		if (Application.platform == RuntimePlatform.Android)
+		{
+			if(Input.GetKeyDown(KeyCode.Escape))
+			{
+				Application.Quit();
+			}
+		}
+	}
+
+	/* void OnGUI()
+	{
+		//GUI.skin = mainmenugui;
+		if(GUI.Button(new Rect(50,50,100,20),"Hello"))
+			GT.SetActive(true);
+	}*/
+}
